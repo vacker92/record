afc689fc16d0358ee0f345eae952801e80eec159
vacker92 <97898850+vacker92@users.noreply.github.com>
2022-05-03 02:49:28 +0900

    Add files via upload

---
+++ imagecrawling.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import datetime
+import urllib.request
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
+elem = driver.find_element_by_name("q")
+elem.send_keys("강아지")
+elem.send_keys(Keys.RETURN)
+
+def doScrollDown(whileSeconds):
+    start = datetime.datetime.now()
+    end = start + datetime.timedelta(seconds=whileSeconds)
+    while True:
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        time.sleep(1)
+        if datetime.datetime.now() > end:
+            break
+
+count = 1
+doScrollDown(5)
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+for image in images:
+    try:        
+        image.click()
+        time.sleep(2)
+        imgUrl = driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img').get_attribute("src")
+        opener = urllib.request.build_opener()
+        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(imgUrl, "./data/" + str(count) + ".jpg")
+        count = count + 1
+    except:
+        pass
+        
+driver.close()
